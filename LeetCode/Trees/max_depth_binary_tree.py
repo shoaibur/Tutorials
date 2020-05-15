cf75606e695aded8de99b9bfc4f437fddf99b0d3
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-15 02:19:51 -0500

    Update max_depth_binary_tree.py

--- LeetCode/Trees/max_depth_binary_tree.py
+++ LeetCode/Trees/max_depth_binary_tree.py
@@ -1,1 +1,13 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
+def max_depth_binary_tree(tree):
+    if not tree:
+        return 0
+    left_depth = max_depth_binary_tree(tree.left)
+    right_depth = max_depth_binary_tree(tree.right)
+    return max(left_depth, right_depth) + 1
+    

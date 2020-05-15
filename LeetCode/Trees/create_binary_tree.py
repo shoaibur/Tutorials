4bcecf4227189cb0deb19bd240aae066853c5a3d
Shoaibur Rahman <shoaibeee@gmail.com>
2020-05-14 21:50:39 -0500

    Update create_binary_tree.py

--- LeetCode/Trees/create_binary_tree.py
+++ LeetCode/Trees/create_binary_tree.py
@@ -1,1 +1,13 @@
-
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+    
+tree = Node(1)
+tree.left = Node(2)
+tree.right = Node(3)
+tree.left.left = Node(4)
+tree.left.right = Node(5)
+tree.right.left = Node(6)
+tree.right.right = Node(7)
